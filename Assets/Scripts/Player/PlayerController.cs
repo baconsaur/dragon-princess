b5453e1bea4dc74aca5aaeff3f76a5036f476874
Ash T. Kestrel <tetrapteryxgames@gmail.com>
2017-03-26 21:13:16 -0600

    Rearranged all the things, also controls are less shitty

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float moveSpeed;
+	public float rotationSpeed;
+
+	private GUIController GuiController;
+	private Animator animator;
+	private GameObject target;
+
+	void Start() {
+		GuiController = GameObject.Find("Canvas").GetComponent<GUIController>();
+		animator = GetComponent<Animator>();
+		target = GameObject.Find("Player Target");
+	}
+
+	void Update() {
+		if (GuiController.isActive()) return;
+
+		UpdatePlayer();
+	}
+
+	public void OnTriggerStay(Collider other) {
+		if (!GuiController.isActive() && other.CompareTag("NPC") && Input.GetButtonDown("Fire1")) {
+			NPCController npcController = other.GetComponent<NPCController>();
+			GuiController.StartDialogue(npcController);
+		}
+	}
+
+	private void UpdatePlayer() {
+		bool shouldWalk = transform.position != target.transform.position;
+		bool shouldRun = Input.GetAxis("Vertical") > 0.7f;
+
+		if (animator.GetBool("walking") != shouldWalk) {
+			animator.SetBool("walking", shouldWalk);
+		} else if (animator.GetBool("running") != shouldRun) {
+			animator.SetBool("running", shouldRun);
+		}
+
+		transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * moveSpeed);
+		transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, Time.deltaTime * rotationSpeed);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerTargetController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTargetController : MonoBehaviour {
+	public float moveSpeed;
+	public float rotationSpeed;
+	public bool isRotationFlipped;
+
+	private GameObject player;
+
+	void Start() {
+		player = GameObject.Find("Player");
+		isRotationFlipped = false;
+	}
+
+	void Update() {
+		if (Input.GetAxis("Vertical") != 0) {
+			transform.Translate(Vector3.forward * Mathf.Abs(Input.GetAxis("Vertical")) * Time.deltaTime * moveSpeed);
+
+			if (Input.GetAxis("Vertical") < 0 && !isRotationFlipped) {
+				transform.RotateAround(player.transform.position, Vector3.up, 180);
+				isRotationFlipped = true;
+			}
+		} else {
+			isRotationFlipped = false;
+			transform.position = player.transform.position;
+		}
+
+		if (Input.GetAxis("Horizontal") != 0) {
+			transform.RotateAround(
+				player.transform.position,
+				Vector3.up,
+				Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed);
+		}
+	}
+}
